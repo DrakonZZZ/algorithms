53498b8eee2c194643faad771ad83a0f5771e24e
Praveen P <124328116+DrakonZZZ@users.noreply.github.com>
2024-08-07 14:11:53 +0530

    Trie Autocomplete implementation

---
+++ Javascript algo/question/AutoComplete/data.js
@@ -0,0 +1,14 @@
+export const singers = [
+  'Adele',
+  'Beyoncé',
+  'Bruno Mars',
+  'Ed Sheeran',
+  'Elton John',
+  'Drake',
+  'Lady Gaga',
+  'Taylor Swift',
+  'Katy Perry',
+  'Kendirck',
+  'Kanye west',
+  'Rihanna',
+]
