a9b419dc295bcdb7662e22d71d48d79b5a7a2776
DrakonZZZ <darkprince.selvan@gmail.com>
2024-08-22 12:17:26 +0530

    graph traversal

---
+++ Graphs/Base/BFSgraphMatrix.js
@@ -0,0 +1,30 @@
+const adjacencyMatrix = [
+  [0, 1, 1, 0],
+  [1, 0, 0, 1],
+  [1, 0, 0, 1],
+  [0, 1, 1, 0],
+]
+
+function BFS(matrix, v) {
+  const queue = [v]
+  const result = []
+  // tracking the visited value
+  const visited = new Array(matrix.length).fill(false)
+  while (queue.length) {
+    const current = queue.shift()
+    result.push(current)
+
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[current][i] === 1 && !visited[i]) {
+        visited[i] = true
+        queue.push(i)
+      }
+    }
+  }
+
+  return result
+}
+
+const v = 0
+const bfsResult = BFS(adjacencyMatrix, v)
+console.log(bfsResult)
