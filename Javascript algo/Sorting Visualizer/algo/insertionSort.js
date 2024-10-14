8e0de788f381fc44eba38682cba42c4c65270d36
DrakonZZZ <darkprince.selvan@gmail.com>
2024-10-14 12:16:37 +0530

    insertionSort

---
+++ Javascript algo/Sorting Visualizer/algo/insertionSort.js
@@ -0,0 +1,8 @@
+import { arr3 } from '../script'
+
+export const insertionSort = async () => {
+  const timeDisplay = document.getElementById('InsertionSortingTime') // Get the time display element
+  const startTime = performance.now()
+
+  const bars = document.querySelector('.array-container-3 .array-bar')
+}
--- Javascript algo/Sorting Visualizer/script.js
+++ Javascript algo/Sorting Visualizer/script.js
@@ -2,17 +2,20 @@ import { selectionSort } from './algo/selectionSort.js'
 import { bubbleSort } from './algo/sortingAlgorithm.js'
 
 export let arr1 = [],
-  arr2 = []
+  arr2 = [],
+  arr3 = []
 
 // Function to generate a random array
 const generateArray = (size = 20) => {
   arr1 = []
-  arr2 = [] // Clear the array before generating a new one
+  arr2 = []
+  arr3 = [] // Clear the array before generating a new one
   console.log(arr2)
   for (let i = 0; i < size; i++) {
     arr1.push(Math.floor(Math.random() * 100) + 1)
   }
   arr2 = [...arr1]
+  arr3 = [...arr1]
   displayArray()
 }
 
@@ -29,18 +32,23 @@ const createBar = (value, arrayLength) => {
 const displayArray = () => {
   arrayContainer1.innerHTML = '' // clear the container
   arrayContainer2.innerHTML = ''
+  arrayContainer3.innerHTML = ''
 
   for (let i = 0; i < arr1.length; i++) {
     const bar1 = createBar(arr1[i], arr1.length)
     const bar2 = createBar(arr2[i], arr2.length)
+    const bar3 = createBar(arr1[i], arr3.length)
     arrayContainer1.appendChild(bar1)
     arrayContainer2.appendChild(bar2)
+    arrayContainer3.appendChild(bar3)
   }
 }
 
 // Event listeners
 const arrayContainer1 = document.querySelector('.array-container-1')
 const arrayContainer2 = document.querySelector('.array-container-2')
+const arrayContainer3 = document.querySelector('.array-container-3')
+
 document.getElementById('generateArray').addEventListener('click', function () {
   generateArray()
 })
